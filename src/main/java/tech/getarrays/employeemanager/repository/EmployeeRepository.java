c4f82ba4063dfef7e3c40bfb534a359a6c2dc1eb
Safal Adhikari <safal.adhikari1@outlook.com>
2021-05-23 21:07:38 -0700

    fixes the exception handling in service layer

--- src/main/java/tech/getarrays/employeemanager/repository/EmployeeRepository.java
+++ src/main/java/tech/getarrays/employeemanager/repository/EmployeeRepository.java
@@ -3,9 +3,10 @@ package tech.getarrays.employeemanager.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import tech.getarrays.employeemanager.model.Employee;
 
+import java.util.Optional;
+
 public interface EmployeeRepository extends JpaRepository<Employee, Long> {
 
     void deleteEmployeeById(Long id);
-
-    Optional findEmployeeById(Long id);
+    Optional<Employee> findEmployeeById(Long id);
 }
--- src/main/java/tech/getarrays/employeemanager/service/EmployeeService.java
+++ src/main/java/tech/getarrays/employeemanager/service/EmployeeService.java
@@ -2,6 +2,7 @@ package tech.getarrays.employeemanager.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import tech.getarrays.employeemanager.exception.UserNotFoundException;
 import tech.getarrays.employeemanager.model.Employee;
 import tech.getarrays.employeemanager.repository.EmployeeRepository;
 
